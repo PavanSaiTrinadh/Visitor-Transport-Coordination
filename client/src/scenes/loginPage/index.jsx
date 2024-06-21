b95c1fe3f3249e0e7d3845e3c02fe78e886f790f
PavanSaiTrinadh <21131a05d0@gvpce.ac.in>
2024-06-21 22:44:06 +0530

    Co-authored-by: navyasrik09042004@gmail.com <navyasrik09042004@gmail.com>

--- client/src/scenes/loginPage/index.jsx
+++ client/src/scenes/loginPage/index.jsx
@@ -1,30 +1,29 @@
-import { Box, Typography, useTheme, useMediaQuery } from "@mui/material";
-import Form from "./Form";
+import React from 'react';
+import { Box, Typography, useTheme, useMediaQuery } from '@mui/material';
+import Form from './Form';
+import './login.css'; // Import the CSS file
 
 const LoginPage = () => {
   const theme = useTheme();
-  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const isNonMobileScreens = useMediaQuery('(min-width: 1000px)');
+
   return (
     <Box>
       <Box
         width="100%"
-        backgroundColor={theme.palette.background.alt}
         p="1rem 6%"
         textAlign="center"
       >
-        <Typography fontWeight="bold" fontSize="32px" color="primary">
+        <Typography fontWeight="bold" fontSize="32px" color="white">
           BookMylift
         </Typography>
       </Box>
 
       <Box
-        width={isNonMobileScreens ? "50%" : "93%"}
-        p="2rem"
-        m="2rem auto"
-        borderRadius="1.5rem"
-        backgroundColor={theme.palette.background.alt}
+        className="glossy-box" // Apply the glossy-box class
+        width={isNonMobileScreens ? '50%' : '93%'}
       >
-        <Typography fontWeight="500" variant="h5" sx={{ mb: "1.5rem" }}>
+        <Typography fontWeight="500" variant="h5" sx={{ mb: '1.5rem' }}>
           Welcome to BookMylift!
         </Typography>
         <Form />
